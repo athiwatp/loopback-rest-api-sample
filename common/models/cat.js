4a954029799b472a34bd4906442a87c0ee71f460
herdian <herdian@lippox.com>
2017-12-15 11:09:54 +0700

    Add validation for username and password presence

--- common/models/cat.js
+++ common/models/cat.js
@@ -1,14 +1,14 @@
 'use strict';
 
 module.exports = function(Cat) {
-    Cat.validatesLengthOf('name', {
-        min: 8, 
-        max: 20, 
-        message: {
-            min: 'Name is too short, minimum 8 characters',
-            max: 'Name is too long, maximum 20 characters'
-        }
-    });
-    Cat.validatesNumericalityOf('radius', {int: true});
-    Cat.validatesNumericalityOf('duration', {int: true});
+  Cat.validatesLengthOf('name', {
+    min: 8,
+    max: 20,
+    message: {
+      min: 'Name is too short, minimum 8 characters',
+      max: 'Name is too long, maximum 20 characters',
+    },
+  });
+  Cat.validatesNumericalityOf('radius', {int: true});
+  Cat.validatesNumericalityOf('duration', {int: true});
 };
--- common/models/user.js
+++ common/models/user.js
@@ -1,7 +1,7 @@
 'use strict';
 
 module.exports = function(User) {
-    // username and email uniqueness ARE already handled by built-in model
-    // email presence IS already handled by built-in model 
-    User.validatesPresenceOf('username', 'password');
+  // username and email uniqueness ARE already handled by built-in model
+  // email presence IS already handled by built-in model
+  User.validatesPresenceOf('username', 'password');
 };
