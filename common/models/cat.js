0519aab7b8203ffb46c1d85c603311250becb70d
herdian <herdian@lippox.com>
2017-12-15 17:15:26 +0700

    Add pm2 ecosystem config and pagination for cat

--- common/models/cat.js
+++ common/models/cat.js
@@ -1,6 +1,7 @@
 'use strict';
 
 module.exports = function(Cat) {
+  // Validation
   Cat.validatesLengthOf('name', {
     min: 8,
     max: 20,
@@ -11,4 +12,29 @@ module.exports = function(Cat) {
   });
   Cat.validatesNumericalityOf('radius', {int: true});
   Cat.validatesNumericalityOf('duration', {int: true});
+
+  // Set default limit and skip for pagination
+  Cat.beforeRemote('find', function(ctx, cat, next) {
+    if (!ctx.args.filter || !ctx.args.filter.limit) {
+      if (!ctx.args.filter) ctx.args.filter = {};
+      ctx.args.filter.limit = 10;
+    }
+    if (!ctx.args.filter || !ctx.args.filter.skip) {
+      if (!ctx.args.filter) ctx.args.filter = {};
+      ctx.args.filter.skip = 0;
+    }
+    next();
+  });
+
+  // Include total item information in response
+  Cat.afterRemote('find', function(ctx, cat, next) {
+    Cat.count(ctx.args.filter.where || {}, function(err, result) {
+      if (err) return next(err);
+      ctx.result = {
+        total: result,
+        rows: ctx.result,
+      };
+      next();
+    });
+  });
 };
