de1beb8ddb8d24eae62aded5c09685f5984f4bfb
herdian <herdian@lippox.com>
2017-12-15 11:06:03 +0700

    Add validation for username and password presence

--- common/models/user.js
+++ common/models/user.js
@@ -1,5 +1,7 @@
 'use strict';
 
 module.exports = function(User) {
-
+    // username and email uniqueness ARE already handled by built-in model
+    // email presence IS already handled by built-in model 
+    User.validatesPresenceOf('username', 'password');
 };
